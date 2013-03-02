cc5d218ebfa47886341adcc371487f9ec03f1f22
Srikanth Agaram <ak@srikanthak.name>
2013-03-02 15:37:44 -0800

    Complete style update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Site::Application.routes.draw do
   resources :articles, :only => [:index, :show]
+  match '/code', :to => 'pages#code', :as => 'code', :via => :get
+  match '/publications', :to => 'pages#publications', :as => 'publications', :via => :get
   root :to => 'articles#index', :via => :get
 end
--- public/files/wireless.conf.rb
+++
@@ -1,8 +0,0 @@
-Settings = {
-	'device' => 'eth1'
-}
-
-KnownList = {
-	'ssid1' => '',
-	'ssid2' => '11111111111111111111111111'
-}
